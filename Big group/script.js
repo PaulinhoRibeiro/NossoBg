2e2b5e88bcc70c35985f0847dccf8756121922e9
PaulinhoRibeiro <165563996+PaulinhoRibeiro@users.noreply.github.com>
2024-11-25 19:47:26 -0300

    Add files via upload

---
+++ Big group/script.js
@@ -0,0 +1,19 @@
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Mensagem enviada com sucesso!');
+});
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
+document.querySelectorAll('nav a').forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+});
